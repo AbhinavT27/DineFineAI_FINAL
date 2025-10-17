c62a334ef0c074ac5fd8c3efc33b6a27e3469c95
AbhinavT27 <abhinav27.tammana@gmail.com>
2025-10-16 19:31:36 -0700

    Update maintenance.ts

--- src/config/maintenance.ts
+++ src/config/maintenance.ts
@@ -3,7 +3,7 @@
 
 export const maintenanceConfig = {
   // Toggle this to enable/disable maintenance mode
-  isUnderConstruction: false,
+  isUnderConstruction: true,
   
   // Set the date when users should check back (e.g., "January 15, 2025")
   checkBackDate: "October 17, 2025",
